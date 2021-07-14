2fce046d58efb2acfd8527871f2a9f5953e82d8a
Asiful Nobel <asif.nobel@gmail.com>
2021-07-15 02:05:36 +0600

    Update script to get user input

--- scripts/usd_bdt_converter.rb
+++ scripts/usd_bdt_converter.rb
@@ -1,5 +1,9 @@
 usd_bdt_conversion_rate = 84.77
-usd = 1000
+
+print "Enter USD Amount -> "
+
+user_input = gets
+usd = user_input.to_i
 bdt = usd * usd_bdt_conversion_rate
 
 puts "#{usd} USD = #{bdt} BDT"
