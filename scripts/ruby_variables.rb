51f8710b0283a095443974c05a49699544dd3bfe
Asiful Nobel <asif.nobel@gmail.com>
2021-07-20 21:16:11 +0600

    Add script about variables

---
+++ scripts/ruby_variables.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+def change_string_to_tsundere(input_string)
+  input_string.replace('tsundere') # frozen string error
+  input_string
+end
+
+hello = 'hello'
+
+begin
+  puts change_string_to_tsundere(hello)
+rescue FrozenError
+  puts "Frozen error - hello: #{hello}"
+end
+
+# Trying duplication
+hello = 'hello'
+puts "Without error - hello:#{change_string_to_tsundere(hello.dup)}" # No frozen error
+puts "hello: #{hello}"
+
+# Ruby first checks if a identifier is a keyword.
+# Then if it is a local variable.
+# Then if it is a method call.
